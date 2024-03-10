7808e43d501ce2e9946626118790f48c20e70138
Agustin Morro <agustin.morro@gmail.com>
2024-03-10 20:03:56 -0300

    fixchessboard

--- src/pages/Chessboard.jsx
+++ src/pages/Chessboard.jsx
@@ -148,7 +148,7 @@ function Chessboard1() {
             <button
               id="btn-game"
               key={index}
-              className=" min-w-64 z-100 mb-4 inline-flex h-8 items-center justify-center font-semibold text-neutral-500 md:text-lg md:w-64 md:h-8"
+              className=" min-w-64 z-100 mb-4 inline-flex h-8 items-center justify-center text-lg font-semibold text-neutral-500 md:text-lg md:w-64 md:h-8"
             >
               {game}
             </button>
